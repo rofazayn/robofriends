96ef198cddf67e269fb305a894b032891e5f1ee6
Abderraouf Zine <rofazayn@gmail.com>
2019-05-23 08:47:39 +0100

    build app

---
+++ build/precache-manifest.9a59ab8ac839333c2a7e16aba5f4da2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95d8a5f79c86d35ec9a5e94b86560a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d6068b53c4958ad0d96",
+    "url": "/static/css/main.6ce60d51.chunk.css"
+  },
+  {
+    "revision": "398c393809c1926129e9",
+    "url": "/static/js/2.934963ba.chunk.js"
+  },
+  {
+    "revision": "9d6068b53c4958ad0d96",
+    "url": "/static/js/main.1487bfcb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
